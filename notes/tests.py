fbfee768f381cf18e7cb05fe424df4ce91591912
github-1403 <fllstck.shp@gmail.com>
2024-05-22 23:55:25 +0330

    add setUp method in NoteListViewTest class

--- notes/tests.py
+++ notes/tests.py
@@ -5,6 +5,9 @@ from .models import Note
 
 class NoteListViewTest(TestCase):
 
+    def setUp(self):
+        self.note1 = Note.objects.create(author='sample_author_1', text='sample_text_1')
+
     def test_notes_list_view_url(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -14,7 +17,6 @@ class NoteListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_notes_list_page(self):
-        note1 = Note.objects.create(author='sample_author_1', text='sample_text_1')
         response = self.client.get(reverse('notes_list'))
-        self.assertContains(response, note1.author)
-        self.assertContains(response, note1.text)
+        self.assertContains(response, self.note1.author)
+        self.assertContains(response, self.note1.text)

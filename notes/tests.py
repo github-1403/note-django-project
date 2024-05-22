0fc7fdfaa61b968e4adcc60c7f9e222e77f27dfd
github-1403 <fllstck.shp@gmail.com>
2024-05-22 23:53:24 +0330

    write test for notes list page which contain text and author of note

--- notes/tests.py
+++ notes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import reverse
+from .models import Note
 
 
 class NoteListViewTest(TestCase):
@@ -11,3 +12,9 @@ class NoteListViewTest(TestCase):
     def test_notes_list_view_url_by_name(self):
         response = self.client.get(reverse('notes_list'))
         self.assertEqual(response.status_code, 200)
+
+    def test_notes_list_page(self):
+        note1 = Note.objects.create(author='sample_author_1', text='sample_text_1')
+        response = self.client.get(reverse('notes_list'))
+        self.assertContains(response, note1.author)
+        self.assertContains(response, note1.text)
